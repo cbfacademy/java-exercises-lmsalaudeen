3d894bc5b4881cf986216438fd5a05ba11fdab7b
Latifah Mojisola Salaudeen <lmsalaudeen@gmail.com>
2023-08-27 18:00:46 +0100

    completed input-output exercises

--- exercises/input-output/src/main/java/com/cbfacademy/App.java
+++ exercises/input-output/src/main/java/com/cbfacademy/App.java
@@ -7,6 +7,11 @@ import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
 
 public class App {
     public static void main(String[] args) {
@@ -33,6 +38,35 @@ public class App {
             streamWriter.close();
             fileOutputStream.close();
 
+        } catch (IOException e) {
+            System.out.println("No file found");
+        }
+
+        // Using .nio
+        String invertedPath = "exercises/input-output/src/main/resources/inverted.txt";
+        // try (Stream<String> stream = Files.lines(Path.of(path))) {
+        //     // stream.forEach(System.out::println);
+        //     // Path copy = Paths.get(newPath);
+        //     // Files.createFile(Path.of(newPath));
+        //     Files.write(Path.of(newPath), (Iterable<String>)stream::iterator)
+            
+        // } catch (IOException e) {
+        //     System.out.println("No file found");
+        // }
+
+        try {
+            List<String> textList = Files.readAllLines(Path.of(path));
+            Collections.reverse(textList);
+            Files.write(Path.of(invertedPath), textList);
+
+            // using an iterator
+            ListIterator<String> textIterator = textList.listIterator(textList.size());
+            while (textIterator.hasPrevious()) {
+            //     writer.write(textIterator.previous());
+            //     writer.newLine();
+                System.out.println(textIterator.previous());
+            }
+            
         } catch (IOException e) {
             System.out.println("No file found");
         }
